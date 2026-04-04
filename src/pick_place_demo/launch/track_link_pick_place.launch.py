d3534480efd63c57af0b4b4bb2a3b89eb1f461aa
ywu2014 <yejunwu123@gmail.com>
2026-04-04 09:24:11 +0800

    feat: track_link launch启动文件实现

---
+++ src/pick_place_demo/launch/track_link_pick_place.launch.py
@@ -0,0 +1,64 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+
+    robot_config_file_name = LaunchConfiguration('robot_config_file_name', default='robot_franka_config.yaml')
+    mjcf_name = LaunchConfiguration("mjcf_name", default='scene_track_link_pick_place.xml')
+
+    # franka moveit 节点
+    incl_franka_robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('panda_moveit_config'), 'launch', 'demo.launch.py')
+        ),
+    )
+
+    # 路径规划节点
+    path_planning_node = Node(
+        package="pick_place_demo",
+        executable="path_planning",
+        output='both',
+        parameters=[{
+            'robot_config_file_name': robot_config_file_name
+        }]
+    )
+
+    # 路径规划桥接节点
+    path_planning_bridge = Node(
+        package="pick_place_demo",
+        executable="path_planning_bridge",
+        output='both'
+    )
+
+    # mujoco仿真节点
+    sim_mujoco_node = Node(
+        package="pick_place_demo",
+        executable="sim_mujoco_pick_place_v2",
+        output='both',
+        parameters=[{
+            'mjcf_name': mjcf_name
+        }]
+    )
+
+    # static tf广播
+    static_tf_publish = Node(
+        package="pick_place_demo",
+        executable="static_tf_publish",
+        output='both'
+    )
+
+    return LaunchDescription(
+        [
+            incl_franka_robot,
+            path_planning_node,
+            path_planning_bridge,
+            sim_mujoco_node,
+            static_tf_publish,
+        ]
+    )
